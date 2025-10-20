4b45692bdea2094e1dba0a7a740e99b51abf0a73
Michael Vessia <michael@vessia.net>
2025-10-19 22:53:25 -0400

    break out options

--- modules/programs/nvf/default.nix
+++ modules/programs/nvf/default.nix
@@ -18,25 +18,6 @@ inputs,
     # most settings are documented in the appendix
     settings = {
       vim = {
-        options =
-          let
-            indentWidth = 2;
-          in
-            {
-            autoindent = true;
-            shiftwidth = indentWidth;
-            tabstop = indentWidth;
-
-            # backup & swap
-            backup = false;
-            writebackup = false;
-            swapfile = false;
-
-            # mouse
-            mouse = "nv";
-            mousemodel = "extend";
-          };
-
         clipboard = {
           enable = true;
           registers = "unnamedplus";
---
+++ modules/programs/nvf/options.nix
@@ -0,0 +1,23 @@
+{
+  programs.nvf.settings.vim = {
+    options =
+      let
+        indentWidth = 2;
+      in
+        {
+        autoindent = true;
+        shiftwidth = indentWidth;
+        tabstop = indentWidth;
+
+        # backup & swap
+        backup = false;
+        writebackup = false;
+        swapfile = false;
+
+        # mouse
+        mouse = "nv";
+        mousemodel = "extend";
+      };
+
+  };
+}
