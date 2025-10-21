5dad0c24446910b09d9cae73fdeac17e6d82d2c0
Michael Vessia <michael@vessia.net>
2025-10-20 22:26:51 -0400

    Add neogit cmd

--- modules/programs/nvf/git.nix
+++ modules/programs/nvf/git.nix
@@ -4,6 +4,26 @@
       enable = true;
       gitsigns.enable = true;
       gitsigns.codeActions.enable = false;
+      neogit = {
+        enable = true;
+        mappings.open = "<leader>G";
+      };
     };
+
+    keymaps = [
+      {
+        key = "<leader>gB";
+        mode = "n";
+        action = ":Gitsigns blame<CR>";
+        desc = "[G]it [B]lame";
+      }
+      {
+        key = "<leader>go";
+        mode = ["n" "x"];
+        lua = true;
+        action = "function() Snacks.gitbrowse() end";
+        desc = "[G]it Browse ([O]pen)";
+      }
+    ];
   };
 }
--- modules/programs/nvf/plugins.nix
+++ modules/programs/nvf/plugins.nix
@@ -1,5 +1,11 @@
 {
   programs.nvf.settings.vim = {
     autocomplete.blink-cmp.enable = true;
+    utility.snacks-nvim = {
+      enable = true;
+      setupOpts = {
+        gitbrowse.enable = true;
+      };
+    };
   };
 }
