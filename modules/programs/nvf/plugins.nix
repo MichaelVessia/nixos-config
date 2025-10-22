e7bc3af50a0a8592bb9265278c1508e6a74e0959
Michael Vessia <michael@vessia.net>
2025-10-22 14:52:04 -0400

    Add dashboard and explorer

--- modules/programs/nvf/plugins.nix
+++ modules/programs/nvf/plugins.nix
@@ -7,6 +7,7 @@
         gitbrowse.enable = true;
         notifier.enable = true;
         dashboard.enable = false;
+        explorer.enable = true;
       };
     };
 
@@ -17,7 +18,6 @@
           { section = "header" },
           { section = "keys", gap = 1, padding = 1 },
           {
-            pane = 2,
             icon = " ",
             desc = "Browse Repo",
             padding = 1,
@@ -26,47 +26,24 @@
               Snacks.gitbrowse()
             end,
           },
-          function()
-            local in_git = Snacks.git.get_root() ~= nil
-            local cmds = {
-              {
-                title = "Open Issues",
-                cmd = "gh issue list -L 3",
-                key = "i",
-                action = function()
-                  vim.fn.jobstart("gh issue list --web", { detach = true })
-                end,
-                icon = " ",
-                height = 7,
-              },
-              {
-                icon = " ",
-                title = "Open PRs",
-                cmd = "gh pr list -L 8",
-                key = "P",
-                action = function()
-                  vim.fn.jobstart("gh pr list --web", { detach = true })
-                end,
-                height = 12,
-              },
-              {
-                icon = " ",
-                title = "Git Status",
-                cmd = "git --no-pager diff --stat -B -M -C",
-                height = 10,
-              },
-            }
-            return vim.tbl_map(function(cmd)
-              return vim.tbl_extend("force", {
-                pane = 2,
-                section = "terminal",
-                enabled = in_git,
-                padding = 1,
-                ttl = 5 * 60,
-                indent = 3,
-              }, cmd)
-            end, cmds)
-          end,
+          {
+            icon = " ",
+            desc = "Browse Issues",
+            padding = 1,
+            key = "i",
+            action = function()
+              vim.fn.jobstart("gh issue list --web", { detach = true })
+            end,
+          },
+          {
+            icon = " ",
+            desc = "Browse PRs",
+            padding = 1,
+            key = "p",
+            action = function()
+              vim.fn.jobstart("gh pr list --web", { detach = true })
+            end,
+          },
         },
       }
       vim.api.nvim_create_autocmd("UIEnter", {
@@ -77,6 +54,14 @@
           end
         end,
       })
+
+      vim.keymap.set("n", "<leader>d", function()
+        require("snacks.dashboard").open()
+      end, { desc = "Open Dashboard" })
+
+      vim.keymap.set("n", "<leader>e", function()
+        require("snacks").explorer()
+      end, { desc = "Explorer" })
     '';
   };
 }
