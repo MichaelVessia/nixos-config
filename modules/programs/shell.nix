1a0ee610f805398995898b348db821bbdd71b5c5
Michael Vessia <michael@vessia.net>
2025-10-26 09:53:43 -0400

    Add precommit formatting

--- modules/programs/shell.nix
+++ modules/programs/shell.nix
@@ -6,12 +6,14 @@
   # starship - an customizable prompt for any shell
   programs.starship = {
     enable = true;
+    enableBashIntegration = true;
     # custom settings
     settings = {
       add_newline = false;
       aws.disabled = true;
       gcloud.disabled = true;
       line_break.disabled = true;
+      package.disabled = true;
     };
   };
 
