72bd832de828d15c228456626965bd1c1c170923
Michael Vessia <michael@vessia.net>
2025-12-28 12:54:26 -0500

    fmcal: add wrapper to load secrets from env file

--- modules/programs/fmcal.nix
+++ modules/programs/fmcal.nix
@@ -1,5 +1,9 @@
 # fmcal - CLI calendar viewer
 #
+# Requires ~/.secrets.env with:
+#   FMCAL_USERNAME=your@email.com
+#   FMCAL_PASSWORD=your-app-password
+#
 # Commands:
 #   fmcal          # show today's events
 #   fmcal -h       # help
@@ -9,6 +13,12 @@
   ...
 }: let
   fmcal-pkg = fmcal.packages.${pkgs.system}.default;
+  wrapper = pkgs.writeShellScriptBin "fmcal" ''
+    set -a
+    source "$HOME/.secrets.env"
+    set +a
+    exec ${fmcal-pkg}/bin/fmcal "$@"
+  '';
 in {
-  home.packages = [fmcal-pkg];
+  home.packages = [wrapper];
 }
